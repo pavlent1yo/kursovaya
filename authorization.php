009513a072159a01c7d5f62a690b09a8c5299b72
pavlent1yo <130046065+pavlent1yo@users.noreply.github.com>
2023-05-17 14:40:43 +0400

    Update and rename authorisation.php to authorization.php

--- authorization.php
+++ authorization.php
@@ -5,17 +5,18 @@
     <title>registration</title>
     <link rel="stylesheet" href="/css/regstyle.css">
     <link rel="shortcut icon" type="image/png" href="img/favicon.png">
-  </head>
+ </head>
 <body>
     <div class="regform">
-      <form action="/vendor/auth.php" method="post" enctype="multipart/form-data">
+      <form action="/registration/login.php" method="post" enctype="multipart/form-data">
+        <a href="/index.php" class="btn btn--reg">Extours</a>
         <label>Логин</label>
         <input type="text" name="login" placeholder="Введите логин">
         <label>Пароль</label>
         <input type="password" name="pass" placeholder="Введите пароль">
         <button type="submit">Подтвердить</button>
         <p>
-        Нет аккаунта? - <a href="/register.php">Регистрация</a>!
+        Нет аккаунта? - <a href="/registration.php">Регистрация</a>!
         </p>
     </div>
 </body>
